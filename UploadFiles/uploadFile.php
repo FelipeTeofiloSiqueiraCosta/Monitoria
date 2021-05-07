6144cde3916f04be30e07d63437679343847eb3e
FelipeTeofiloSiqueiraCosta <felipeteofilosiqueiracosta@gmail.com>
2021-05-07 14:09:39 -0300

    07052021.4

--- UploadFiles/uploadFile.php
+++ UploadFiles/uploadFile.php
@@ -32,12 +32,12 @@ if ( isset( $_FILES[ 'arquivo' ][ 'name' ] ) && $_FILES[ 'arquivo' ][ 'error' ] 
   // Aqui eu enfileiro as extensões permitidas e separo por ';'
   // Isso serve apenas para eu poder pesquisar dentro desta String
   if ( strstr ( '.jpg;.jpeg;.gif;.png', $extensao ) ) {
-      // Cria um nome único para esta imagem
+      // Cria um nome único para esta imagem 
       // Evita que duplique as imagens no servidor.
       // Evita nomes com acentos, espaços e caracteres não alfanuméricos
       $novoNome = uniqid ( time () ) . '.' . $extensao;
 
-      // Concatena a pasta com o nome ------------------------ aaaaa
+      // Concatena a pasta com o nome ------------------------ aaaaabbbb
       $destino =  './Images/'.$novoNome;
       // echo "<br><br>Arquivo: $arquivo_tmp";
       // echo "<br><br>DESTINO: ".$destino;
