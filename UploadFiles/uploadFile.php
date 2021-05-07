a1a235c31f5ef99b8d92a4d651891ca3ecc03fd2
FelipeTeofiloSiqueiraCosta <felipeteofilosiqueiracosta@gmail.com>
2021-05-07 13:18:00 -0300

    07052001.2

--- UploadFiles/uploadFile.php
+++ UploadFiles/uploadFile.php
@@ -32,7 +32,7 @@ if ( isset( $_FILES[ 'arquivo' ][ 'name' ] ) && $_FILES[ 'arquivo' ][ 'error' ] 
   // Aqui eu enfileiro as extensões permitidas e separo por ';'
   // Isso serve apenas para eu poder pesquisar dentro desta String
   if ( strstr ( '.jpg;.jpeg;.gif;.png', $extensao ) ) {
-      // Cria um nome único para esta imagem asd
+      // Cria um nome único para esta imagem
       // Evita que duplique as imagens no servidor.
       // Evita nomes com acentos, espaços e caracteres não alfanuméricos
       $novoNome = uniqid ( time () ) . '.' . $extensao;
