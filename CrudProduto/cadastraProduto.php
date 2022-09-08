1f55797e82499b991cdc376a6a7030700e5ff265
Felipe Teofilo Siqueira Costa <felipeteofilosiqueiracosta@gmail.com>
2022-09-08 11:06:14 -0300

    Update cadastraProduto.php

--- CrudProduto/cadastraProduto.php
+++ CrudProduto/cadastraProduto.php
@@ -1,13 +1,15 @@
 <?php
 include './connection.php';
 
-$desc = $_POST['descricao'];
-$qte = $_POST['estoque'];
-$valor = $_POST['preco'];
+$desc = $_POST['descricao']; //pegando descrição do formulário
+$qte = $_POST['estoque']; //pegando estoque do formulário
+$valor = $_POST['preco']; //pegando preco do formulário
 
 echo "descricao: ".$desc." qte em estoque: ".$qte." Preço: ".$valor;
 
 $comando = mysqli_query($conexao,"insert into produto(descricao,qteEstoque,preco) values('$desc',$qte,$valor)");
+
+//o retorno da execução do insert é booleano (retorna true ou false)
 
 if($comando){
   echo"<h1>Produto inserido com Sucesso!!!</h1>";
